b0fd938bd15838cb4c5fa00a4e74ade9d2ec9369
Saidulloh <ssavutokhunov@gmail.com>
2022-12-24 13:43:34 +0600

    create view for model User

---
+++ apps/users/views.py
@@ -0,0 +1,20 @@
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin
+
+from apps.users.models import User
+from apps.users.serialisers import UserCreateSerializer
+
+
+class UserApiViewSet(GenericViewSet,
+                    CreateModelMixin,
+                    ListModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserCreateSerializer
+
+
+class UserDetailApiViewSet(GenericViewSet,
+                        RetrieveModelMixin,
+                        UpdateModelMixin,
+                        DestroyModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserCreateSerializer
