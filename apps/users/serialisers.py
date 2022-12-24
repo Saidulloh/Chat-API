61ffc8267430429130dd70fb71e86f8c678d454d
Saidulloh <ssavutokhunov@gmail.com>
2022-12-24 13:43:19 +0600

    create serializer for model User

---
+++ apps/users/serialisers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from apps.users.models import User
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'avatarka',
+            'bio',
+            'email',
+            'create_at',
+            'phone_number',
+            'age',
+            'password'
+        )
+
+    def create(self, validated_data):
+        password = validated_data['password']
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
