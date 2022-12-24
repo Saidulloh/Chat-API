71a60261d5534badad5ce841ecfa961b82037e2a
Saidulloh <ssavutokhunov@gmail.com>
2022-12-24 17:05:26 +0600

    add permission IsOwner

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin
 
 from apps.users.models import User
+from apps.users.permissions import IsOwner
 from apps.users.serialisers import UserCreateSerializer
 
 
@@ -18,3 +19,4 @@ class UserDetailApiViewSet(GenericViewSet,
                         DestroyModelMixin):
     queryset = User.objects.all()
     serializer_class = UserCreateSerializer
+    permission_classes = [IsOwner]
