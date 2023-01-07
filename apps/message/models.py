0c0f49ab5323fa4a13858a4c32323afaee662098
Saidulloh <ssavutokhunov@gmail.com>
2023-01-07 06:11:52 +0600

    create model Message

--- apps/message/models.py
+++ apps/message/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+from apps.users.models import User
+
+
+class Message(models.Model):
+    """
+    Model for messages
+    """
+    text = models.TextField(
+        verbose_name='text'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='created_at'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='updated_at'
+    )
+    owner = models.ManyToManyField(
+        User,
+        verbose_name='message_owner'
+    )
+    receiver = models.ManyToManyField(
+        User,
+        verbose_name='receiver'
+    )
+    is_read = models.BooleanField(
+        default=False,
+        verbose_name='is_read'
+    )
+
+    def __str__(self):
+        return f'{self.id}'
+
+    class Meta:
+        verbose_name = 'message'
+        verbose_name_plural = 'Messages'
