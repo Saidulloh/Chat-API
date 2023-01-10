44dab95e27d8aea48d5b639c4501bd1a0bfe1838
Saidulloh <ssavutokhunov@gmail.com>
2023-01-10 21:50:49 +0600

    created CRUD for messages

--- apps/message/serialisers.py
+++ apps/message/serialisers.py
@@ -7,3 +7,10 @@ class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         read_only_fields = ('owner', )
+        fields = (
+            'id',
+            'text',
+            'created_at',
+            'chat',
+            'is_read'
+        )
