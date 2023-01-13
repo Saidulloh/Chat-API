15a8cd9078ba4131ddb7871bd9f7ace9576e3b3d
Saidulloh <ssavutokhunov@gmail.com>
2023-01-14 02:23:18 +0600

    done functionality for users

--- apps/users/serialisers.py
+++ apps/users/serialisers.py
@@ -3,20 +3,13 @@ from rest_framework import serializers
 from apps.users.models import User
 
 
+fields = ['id', 'username', 'avatarka', 'bio', 'email', 'created_at', 'phone_number', 'age']
+
+
 class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = (
-            'id',
-            'username',
-            'avatarka',
-            'bio',
-            'email',
-            'created_at',
-            'phone_number',
-            'age',
-            'password'
-        )
+        fields = fields
 
     def create(self, validated_data):
         password = validated_data['password']
@@ -24,3 +17,28 @@ class UserCreateSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+
+class UserChatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = fields
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'avatarka',
+            'bio',
+            'email',
+            'phone_number',
+            'age'
+        )
+
+
+class GetUserInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = fields + ['password']
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,14 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from apps.users.models import User
 from apps.users.permissions import IsOwner
-from apps.users.serialisers import UserCreateSerializer
+from apps.users.serialisers import UserCreateSerializer, UserDetailSerializer, GetUserInfoSerializer
+from apps.chats.models import Chat
 
 
 class UserApiViewSet(GenericViewSet,
@@ -18,5 +23,27 @@ class UserDetailApiViewSet(GenericViewSet,
                            UpdateModelMixin,
                            DestroyModelMixin):
     queryset = User.objects.all()
-    serializer_class = UserCreateSerializer
+    serializer_class = UserDetailSerializer
     permission_classes = [IsOwner]
+
+    @action(
+        detail=False, permission_classes=[IsAuthenticated], methods=["get"]
+    )
+    def current_user(self, request, email=None):
+        serializer = GetUserInfoSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(
+        detail=False, permission_classes=[IsAuthenticated], methods=["get"]
+    )
+    def another_user(self, request, email=None):
+        lst = []
+        chats = Chat.objects.filter(members__in=[request.user])
+        for user in User.objects.all():
+            for chat in chats:
+                if user in chat.members.all() or user == request.user:
+                    continue
+                lst.append(user)
+        users = User.objects.filter(id__in=[i.id for i in set(lst)])
+        serializer = UserDetailSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
